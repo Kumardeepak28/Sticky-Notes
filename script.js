32fc5ef724a37656f27ac9dd2be19cc301a60358
Deepak <kumardeepakbr28@gmail.com>
2024-01-17 17:29:40 +0530

    notes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let container = document.querySelector(".text-and-color").children;
+let ct = document.querySelector(".color_and_text").children;
 
-let textAreaValue = container[0].value;
-let colorValue = container[1].children[0].value;
+// console.log(colorValue)
+// let textAreaValue = container[0].value;
+// let colorValue = container[1].children[0];
 //console.log(colorValue);
 
 let btn = document.querySelector("button");
@@ -9,21 +10,44 @@ let btn = document.querySelector("button");
 
 let rightContainer = document.querySelector(".right-container-notes");
 
-btn.addEventListener("click",()=>{
-    if(textAreaValue === "") {
+let btn2 = document.querySelectorAll(".btn2");
+
+
+btn.addEventListener("click", ()=>{
+    let colorValue = ct[1].children[1].value;
+// let colorValue = document.querySelector('.color_and_text input[type="color"]').value;
+
+    console.log(colorValue)
+    if(ct[0].value === "") {
         alert("please write some text");
         return;
     }
+console.log(ct);
 
-    makeStickyNote(textAreaValue,colorValue);
-})
+    makeStickyNote(ct[0].value,colorValue);
+
+    ct[0].value = "";
+    document.querySelector(".notesNotAdded").style.display = "none";
+    removeStickyNotes()
+});
+
+ function removeStickyNotes() {
+    
+    btn2 = document.querySelectorAll(".btn2");
+
+    btn2.forEach((singleBtn)=>{
+        singleBtn.addEventListener("click", (e)=>{
+              e.target.parentElement.remove();
+        });
+    });
+}
 
 function makeStickyNote(text,color) {
+    // console.log(text,color);
+    
     let div = document.createElement("div");
-    div.innerHTML = `
-    <p>${text}</p>
-    <button>X</button>
-    `;
+    div.classList.add("output")
+    div.innerHTML = `<p>${text}</p><button class="btn2">*</button>`;
     div.style.backgroundColor = color;
     rightContainer.appendChild(div);
 }
